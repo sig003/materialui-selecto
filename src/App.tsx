925e94ac69072c7fce38406e64ecae9cb21dc562
sig03 <sig03@jiran.com>
2021-08-30 01:07:12 +0000

    first commit - material-ui's Lisitem components - selecto module

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MouseEvent, useState, useEffect } from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import './style.css';
+import Selecto from "react-selecto";
 
 function App() {
+  const list = ['aaaaaa', 'bbbbbb', 'cccccc', 'dddddd', 'eeeeee', 'ffffff', 'gggggg', 'hhhhhh', 'iiiiii', 'jjjjjj', 'kkkkkk'];
+  const [val, setVal] = useState(['']);
+
+  const handleMouseOver = (e: MouseEvent) => {
+    const target: HTMLElement = e.target as HTMLElement;
+    target.classList.add('Mui-focusVisible');
+  }
+  const handleMouseOut = (e: MouseEvent) => {
+    const target: HTMLElement = e.target as HTMLElement;
+    target.classList.remove('Mui-focusVisible');    
+  }
+
+  useEffect(() => {
+    console.log(val);
+  },[val])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        {list.map(i => 
+            <>
+              <ListItem 
+                key={i}
+                className='list'
+                onMouseOver={handleMouseOver}                                  
+                onMouseOut={handleMouseOut}
+              >
+                <ListItemText primary={i}/>
+                <div style={{display:'none'}}>{i}</div>
+              </ListItem>
+              <Divider />
+            </>
+        )}
+      </div>
+
+      <Selecto
+          dragContainer={".container"}
+          selectableTargets={[".list"]}
+          hitRate={0}
+          selectByClick={true}
+          selectFromInside={true}
+          ratio={0}
+          onSelect={e => {
+              e.added.forEach((el:any, index:number) => {
+                  el.classList.add("selected");
+                  setVal(prev => [...prev, el.innerText])
+                  //setVal([el.getAttribute("key")]);
+                  //console.log(el.innerText)
+                  //console.log(el.attributes)
+              });
+              e.removed.forEach(el => {
+                  el.classList.remove("selected");
+                  setVal([])
+              });
+          }}
+        />
+    </>
   );
 }
 
